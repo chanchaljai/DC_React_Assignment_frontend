5137b416463adcc1983324dd652baf1298651b98
Chanchal Jaiswal <chanchal123jaiswal@gmail.com>
2026-06-24 18:22:59 +0530

    folder setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-3xl text-center mt-3'>Hello, World!</h1>
+    </div>
+  )
+}
+
+export default App
